0032d95ade1a115aa232ab8b11c029225ebdbdea
Egor Mikhailov <egor11022004@gmail.com>
2022-09-19 01:33:21 +0300

    assets for the app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:exercises_tracker/main.dart';
+import 'package:exercises_tracker/login_screen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
