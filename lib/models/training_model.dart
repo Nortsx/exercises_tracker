36e5f9110c06159f843bf7deba8d1f220fe28ec9
Egor Mikhailov <egor11022004@gmail.com>
2022-09-19 00:49:17 +0300

    beg it works

--- lib/models/training_model.dart
+++ lib/models/training_model.dart
@@ -1,9 +1,9 @@
-
 //model for quantity estimated trainings
 class TrainingModel {
-
   //number of completed exercises so far
   late int currentAmount;
+
+  late int currentAmountForProgressBar;
 
   //goal to achieve on daily basis
   late int goalInQuantity;
@@ -11,16 +11,17 @@ class TrainingModel {
   //type of sport activity
   late String type;
 
-
-  SportActivity() {}
+  TrainingModel() {
+    currentAmountForProgressBar = 0;
+    currentAmount = 0;
+    goalInQuantity = 100;
+  }
 
 //get previously saved data from db to restore currentAmount and goal.
   void fetchFromDB() {}
 
-
 //upload each addition of completed excercises alongside their time of completion(year, day, hours, minutes)
   void uploadToDB(int newAmount, DateTime dateTime) {}
-
 
 //return number of completed exercises
   int getCurrentAmount() {
@@ -32,13 +33,17 @@ class TrainingModel {
     goalInQuantity = newGoal;
   }
 
-
 //increase amount of completed exercises by adding amount of ones you've just done
   void increaseBy(int amount, DateTime dateTime) {
+    if (currentAmountForProgressBar + amount > 100) {
+      currentAmountForProgressBar = 100;
+    } else {
+      currentAmountForProgressBar += amount;
+    }
     currentAmount += amount;
-    uploadToDB(amount, dateTime); // upload typeOfSport, date and increased amount
+    uploadToDB(
+        amount, dateTime); // upload typeOfSport, date and increased amount
   }
-
 
 //every 24h reset amount of completed exercises
   void resetCurrentAmount() {
